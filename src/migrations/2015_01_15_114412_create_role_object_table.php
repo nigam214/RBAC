9e141549d90efe0a0dc3c26a2281fbd32a0c485a
Ashish Nigam <ashish@virtupaper.com>
2017-11-03 20:30:40 +0530

    class name mismatch issue fixed

--- src/migrations/2015_01_15_114412_create_role_object_table.php
+++ src/migrations/2015_01_15_114412_create_role_object_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRoleUserTable extends Migration
+class CreateRoleObjectTable extends Migration
 {
     /**
      * Run the migrations.
