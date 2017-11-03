5e6df13a8772296cd50e7837c362eaa985954286
Ashish Nigam <ashish@virtupaper.com>
2017-11-03 23:27:08 +0530

    slug of permission table must be unique

--- src/migrations/2015_01_26_115212_create_permissions_table.php
+++ src/migrations/2015_01_26_115212_create_permissions_table.php
@@ -16,7 +16,7 @@ class CreatePermissionsTable extends Migration
         Schema::create($permissions, function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->string('description')->nullable();
             $table->string('model')->nullable();
             $table->timestamps();
